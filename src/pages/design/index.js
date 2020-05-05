7a972a7d3af8af09b04c9d86ad72793eb938cae8
Negin Sauermann <nuggetnegin@gmail.com>
2020-05-05 14:42:40 -0400

    test commit 👀

--- src/pages/design/index.js
+++ src/pages/design/index.js
@@ -30,7 +30,7 @@ const Design = () => {
       <Nav categories={data.allContentfulCategory.nodes} />
       <main>
         <div className="wrapper">
-          <h1>Design TEST</h1>
+          <h1>Design TESTT</h1>
         </div>
       </main>
     </Layout>
